14841edcf049726b083266c6b4ddbcdb412536e7
h-philip <p.hoennecke@tu-bs.de>
2019-03-14 18:32:16 +0100

    added clientid

--- tradfri/tradfriStatus.py
+++ tradfri/tradfriStatus.py
@@ -46,7 +46,7 @@ def tradfri_get_devices(hubip, securityid):
 def tradfri_get_lightbulb(hubip, securityid, deviceid):
     """ function for getting tradfri lightbulb information """
     tradfriHub = 'coaps://{}:5684/15001/{}' .format(hubip, deviceid)
-    api = '{} -m get -u "Client_identity" -k "{}" "{}" | awk \'NR==4\''.format(coap, securityid,
+    api = '{} -m get -u "{}" -k "{}" "{}" | awk \'NR==4\''.format(coap, clientid, securityid,
                                                                                tradfriHub)
 
     if os.path.exists(coap):
@@ -60,7 +60,7 @@ def tradfri_get_lightbulb(hubip, securityid, deviceid):
 def tradfri_get_groups(hubip, securityid):
     """ function for getting tradfri groups """
     tradfriHub = 'coaps://{}:5684/15004'.format(hubip)
-    api = '{} -m get -u "Client_identity" -k "{}" "{}" | awk \'NR==4\''.format(coap, securityid,
+    api = '{} -m get -u "{}" -k "{}" "{}" | awk \'NR==4\''.format(coap, clientid, securityid,
                                                                                tradfriHub)
 
     if os.path.exists(coap):
@@ -74,7 +74,7 @@ def tradfri_get_groups(hubip, securityid):
 def tradfri_get_group(hubip, securityid, groupid):
     """ function for getting tradfri group information """
     tradfriHub = 'coaps://{}:5684/15004/{}'.format(hubip, groupid)
-    api = '{} -m get -u "Client_identity" -k "{}" "{}" | awk \'NR==4\''.format(coap, securityid,
+    api = '{} -m get -u "{}" -k "{}" "{}" | awk \'NR==4\''.format(coap, clientid, securityid,
                                                                                tradfriHub)
 
     if os.path.exists(coap):
